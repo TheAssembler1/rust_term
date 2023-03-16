9e85e4c462f1d0641bc280dd663b520d13d66eb7
Noah Lewis <47840925+TheAssembler1@users.noreply.github.com>
2023-03-15 21:02:14 -0600

    "gotterDone"

--- src/built_in/mod.rs
+++ src/built_in/mod.rs
@@ -2,10 +2,24 @@ use super::path;
 use std::{env, ops::Add};
 
 pub fn cd(current_dir: String, args: Vec<String>) {
+    let dest_path;
     // NOTE: should be path.. FUCK
-    let path = args.get(1).expect("unable to get second arg");
-    let dest_path = current_dir.add("\\").add(path);
-    env::set_current_dir(dest_path).unwrap();
+    if args.len() > 1 {
+        let path = match args.get(1) {
+            Some(arg) => arg,
+            _ => {
+                eprintln!("invalid path");
+                return;
+            }
+        };
+
+        dest_path = current_dir.add("\\").add(path);
+    } else {
+        dest_path = String::from(env::home_dir().unwrap().to_str().unwrap());
+    }
+    if let Err(_) = env::set_current_dir(dest_path) {
+        eprintln!("invalid path");
+    }
 }
 
 pub fn ls(current_dir: &str) {
